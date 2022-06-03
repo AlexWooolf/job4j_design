bb8845d012c321c1f03422d3174d16d31104d813
AlexWooolf <para_bellum@list.ru>
2022-06-03 15:25:21 +0300

    2. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -1,6 +1,7 @@
 package ru.job4j.collection;
 
-import java.util.Calendar;
+import java.util.*;
+
 
 public class User {
 
@@ -14,5 +15,20 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Calendar calendar = new GregorianCalendar(2000, 0, 0);
+        var user1 = new User("Name", 2, calendar);
+        var user2 = new User("Name", 2, calendar);
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        List<Map.Entry<User, Object>> list = new ArrayList<>(map.entrySet());
+        System.out.println(user1.hashCode());
+        System.out.println(user2.hashCode());
+        for (Map.Entry l : list) {
+            System.out.println(l.getKey() + ":" + l.getValue());
+        }
     }
 }
