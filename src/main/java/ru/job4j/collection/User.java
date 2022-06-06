345e76e35f9fa7a8bfe7a03ecc8ca6b1604767e0
AlexWooolf <para_bellum@list.ru>
2022-06-06 13:09:25 +0300

    2. Без переопределения equals и hashCode [#1005 #308618]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -17,6 +17,11 @@ public class User {
         this.birthday = birthday;
     }
 
+    static int hash(Object key) {
+        int h = key.hashCode();
+        return (key == null) ? 0 : (h) ^ (h >>> 16);
+    }
+
     public static void main(String[] args) {
         Calendar calendar = new GregorianCalendar(2000, 0, 0);
         var user1 = new User("Name", 2, calendar);
@@ -27,6 +32,10 @@ public class User {
         List<Map.Entry<User, Object>> list = new ArrayList<>(map.entrySet());
         System.out.println(user1.hashCode());
         System.out.println(user2.hashCode());
+        int i1 = hash(user1) & 15;
+        int i2 = hash(user2) & 15;
+        System.out.println(i1);
+        System.out.println(i2);
         for (Map.Entry l : list) {
             System.out.println(l.getKey() + ":" + l.getValue());
         }
