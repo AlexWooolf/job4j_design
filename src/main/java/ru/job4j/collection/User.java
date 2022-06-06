dcb47e274f87b453efc242ae2a3c552203fe54bf
AlexWooolf <para_bellum@list.ru>
2022-06-06 16:29:17 +0300

    2. Без переопределения equals и hashCode [#1005 #308618]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -11,6 +11,19 @@ public class User {
 
     private Calendar birthday;
 
+   /* @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }*/
+
     public User(String name, int children, Calendar birthday) {
         this.name = name;
         this.children = children;
@@ -21,14 +34,14 @@ public class User {
         Calendar calendar = new GregorianCalendar(2000, 0, 0);
         var user1 = new User("Name", 2, calendar);
         var user2 = new User("Name", 2, calendar);
-        Map<User, Object> map = new HashMap<>();
+        Map<User, Object> map = new HashMap<>(16);
         map.put(user1, new Object());
         map.put(user2, new Object());
         List<Map.Entry<User, Object>> list = new ArrayList<>(map.entrySet());
         System.out.println(user1.hashCode());
         System.out.println(user2.hashCode());
-        int i1 = user1.hashCode() % 16;
-        int i2 = user2.hashCode() % 16;
+        int i1 = (user1.hashCode() ^ (user1.hashCode() >>> 16)) & (16 - 1);
+        int i2 = (user1.hashCode() ^ (user2.hashCode() >>> 16)) & (16 - 1);
         System.out.println(i1);
         System.out.println(i2);
         for (Map.Entry l : list) {
