9dcd6d50af141c8c9f5a099bb169deeb16342f5b
AlexWooolf <para_bellum@list.ru>
2022-06-20 15:18:51 +0300

    0.3. BufferedReader. [#252489 #314480]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -2,35 +2,30 @@ package ru.job4j.io;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class LogFilter {
+    private boolean check(String line) {
+        String[] log = line.split(" ");
+        return log[log.length - 2].contains("404");
+    }
+
     public List<String> filter(String file) {
-        List<String[]> log = new ArrayList<>();
+        List<String> log = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
             for (String line = in.readLine(); line != null; line = in.readLine()) {
-                log.add(line.split(" "));
+                if (check(line)) {
+                    log.add(line);
+                }
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        String str = "";
-        List<String> rsl = new ArrayList<>();
-        log = log.stream()
-                .filter(x -> x[x.length - 2].contains("404"))
-                .collect(Collectors.toList());
-        for (String[] string : log) {
-            for (String s : string) {
-                str = str + " " + s;
-            }
-           rsl.add(str);
-            str = "";
-        }
-        return rsl;
+        return log;
     }
+
 
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
