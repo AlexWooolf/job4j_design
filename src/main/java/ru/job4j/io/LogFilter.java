d4c8062c32bbbca58796df16571c99397c7f3643
AlexWooolf <para_bellum@list.ru>
2022-06-20 15:28:52 +0300

    0.3. BufferedReader. [#252489 #314480]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -30,7 +30,8 @@ public class LogFilter {
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("log.txt");
-        System.out.println(log);
-
+        for (String s : log) {
+            System.out.println(s);
+        }
     }
 }
