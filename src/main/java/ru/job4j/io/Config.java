73bd60d377291c64be328291b7b16516082270f5
AlexWooolf <para_bellum@list.ru>
2022-06-21 12:15:12 +0300

    1. Читаем файл конфигурации [#858]

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -3,27 +3,41 @@ package ru.job4j.io;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringJoiner;
+import java.util.*;
 
 public class Config {
 
     private final String path;
-    private final Map<String, String> values = new HashMap<String, String>();
+    private final Map<String, String> values = new HashMap<>();
 
     public Config(final String path) {
         this.path = path;
     }
 
+    /*public void load() {
+        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+            in.lines()
+                    .filter(l -> l.length() > 0 && !l.contains("#"))
+                    .map(l -> l.split("="))
+                    .filter(l -> l.length == 2 && l[0] != null && l[1] != null)
+                    .forEach(l -> values.put(l[0], l[1]));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }*/
+
     public void load() {
+        List<String> l = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(path))) {
             for (String line = in.readLine(); line != null; line = in.readLine()) {
-                String[] str = line.split("=");
-                if () {
+                l.add(line);
+            }
+            for (String s : l) {
+                if (s.length() < 3 || s.contains("#") || !s.contains("=") || s.contains(" ") || s.startsWith("=") || s.endsWith("=")) {
                     throw new IllegalArgumentException();
                 } else {
-                    values.put(str[0], str[1]);
+                    var rsl = s.split("=", 2);
+                    values.put(rsl[0], rsl[1]);
                 }
             }
         } catch (IOException e) {
@@ -52,5 +66,4 @@ public class Config {
         config.load();
         System.out.println(config.values);
     }
-
 }
