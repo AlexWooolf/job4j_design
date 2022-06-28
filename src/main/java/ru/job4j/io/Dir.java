8e2090107cbe445b9c097c28501df7bbb5b1c2e8
AlexWooolf <para_bellum@list.ru>
2022-06-28 17:21:36 +0300

    4.0. File [#252491 #315683]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -13,7 +13,7 @@ public class Dir {
         }
         System.out.println(String.format("size : %s", file.getTotalSpace()));
         for (File subfile : file.listFiles()) {
-            System.out.println(subfile.getAbsoluteFile() + String.format(" size : %s", subfile.length()));
+            System.out.println(subfile.getName() + String.format(". size : %s", subfile.length()));
         }
     }
 }
