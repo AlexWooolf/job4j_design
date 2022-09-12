9a868c9e88caea7b071a802c812926ba642c881d
Vovk <para_bellum@list.ru>
2022-09-12 20:33:15 +0300

    3. Формат XML [#313165]

---
+++ src/main/java/ru/job4j/training/CarsEnum.java
@@ -0,0 +1,16 @@
+package ru.job4j.training;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class  CarsEnum {
+    enum Cars {
+        BMW, AUDI, BENTLEY
+    }
+
+    public static void main(String[] args) {
+        String s = "Hello";
+        s = s.concat(" world");
+        System.out.println(s);
+    }
+}
