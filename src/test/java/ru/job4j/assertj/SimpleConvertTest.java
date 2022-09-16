ace13d2e3b23a972e1d9f763178501757a960675
Vovk <para_bellum@list.ru>
2022-09-16 17:17:03 +0300

    Утверждения с коллекциями [#504885]

--- src/test/java/ru/job4j/assertj/SimpleConvertTest.java
+++ src/test/java/ru/job4j/assertj/SimpleConvertTest.java
@@ -16,4 +16,33 @@ class SimpleConvertTest {
                 .containsAnyOf("zero", "second", "six")
                 .doesNotContain("first", Index.atIndex(1));
     }
+
+    @Test
+    void checkList() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        var list = simpleConvert.toList("first", "second", "three", "four", "five");
+        assertThat(list).hasSize(5)
+                .contains("second")
+                .contains("first", Index.atIndex(0))
+                .containsAnyOf("zero", "second", "six")
+                .doesNotContain("first", Index.atIndex(1));
+    }
+
+    @Test
+    void checkSet() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        var set = simpleConvert.toSet("first", "first", "three", "four", "five");
+        assertThat(set).hasSize(4)
+                .contains("three")
+                .doesNotContain("zero", "second");
+    }
+
+    @Test
+    void checkMap() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        var map = simpleConvert.toMap("first", "second", "first", "four");
+        assertThat(map).hasSize(3)
+                .containsKey("first")
+                .containsValue(0);
+    }
 }
