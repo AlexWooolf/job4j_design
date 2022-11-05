7d80d14191aa45f40079e65dc010196e52885a43
Vovk <para_bellum@list.ru>
2022-11-05 22:18:36 +0300

    2. Триггеры [#504803 #358176]

--- db/trigger.sql
+++ db/trigger.sql
@@ -6,11 +6,12 @@ create table products (
     price integer
 );
 
-create trigger ploti_nologi_trigger
-    after insert on products
-    referencing new table as inserted
-    for each statement
-    execute procedure nologi();
+create table history_of_price (
+    id serial primary key,
+    name varchar(50),
+    price integer,
+    date timestamp
+);
 
 create or replace function nologi()
     returns trigger as
@@ -24,36 +25,16 @@ $$
 $$
 LANGUAGE 'plpgsql';
 
-create trigger ploti_nologi_now_trigger
-    before insert
-    on products
-    for each row
-    execute procedure nologi_now();
-
 create or replace function nologi_now()
     returns trigger as
 $$
     BEGIN
         update products
-        set price = price + price * 0.2
-        where id = new.id;
+        new.price = new.price + new.price * 0.2
         return NEW;
     END;
 $$
 LANGUAGE 'plpgsql';
-
-create table history_of_price (
-    id serial primary key,
-    name varchar(50),
-    price integer,
-    date timestamp
-);
-
-create trigger price_history
-    after instert
-    on products
-    for each row
-    execute procedure history();
 
 create or replace function history()
     returns trigger as
@@ -65,4 +46,27 @@ $$
     END;
 $$
 LANGUAGE 'plpgsql';
+
+
+
+create trigger ploti_nologi_trigger
+    after insert on products
+    referencing new table as inserted
+    for each statement
+    execute procedure nologi();
+
+
+
+create trigger ploti_nologi_now_trigger
+    before insert
+    on products
+    for each row
+    execute procedure nologi_now();
+
+create trigger price_history
+    after insert
+    on products
+    for each row
+    execute procedure history();
+
 
