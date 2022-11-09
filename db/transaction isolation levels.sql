8d2c5c03ff343dd7ac1b3086cf226ebc93796bc6
Vovk <para_bellum@list.ru>
2022-11-10 00:37:59 +0300

    4.3. Транзакции. Уровни изолированности транзакций. [#504812 #359449]

--- db/transaction isolation levels.sql
+++ db/transaction isolation levels.sql
@@ -1,71 +1,63 @@
-create table storage(
+create table new_storage(
 id serial primary key,
-name text
+name text,
+weight int
 );
 
-insert into storage(name)
-values ('one'),
-       ('two'),
-       ('three'),
-	   ('four');
-	   select * from storage;
+insert into new_storage(name, weight)
+values ('one', 1),
+       ('two', 2),
+       ('three', 3),
+	   ('four', 4);
+	   select * from new_storage;
 
 	 --Dirty read. Console 1
 	 show transaction isolation level;
      begin;
-	 update storage
+	 update new_storage
  set name = name || 'one'
  where id = 1;
- select * from storage;
+ select * from new_storage;
  rollback;
 
  --Dirty read. Console 1
 	  begin;
-	  select * from storage;
+	  select * from new_storage;
 	  rollback;
 
 	  --Non repeatable read. Console 1
 	  begin;
-	  update storage
+	  update new_storage
  set name = name || 'one'
  where id = 1;
- select * from storage;
+ select * from new_storage;
 	  commit;
 	  begin; --begin after set new isolation level on console 2
-	  update storage
+	  update new_storage
  set name = name || 'one'
  where id = 1;
- select * from storage;
+ select * from new_storage;
 	  commit;
 
 	   --Non repeatable read. Console 2
 	   begin;
-	   select * from storage; --error
+	   select * from new_storage; --error
 	   commit;
 	   begin transaction isolation level repeatable read;
-	   select * from storage;
-	   select * from storage; --select after commit on console 1 is ok
+	   select * from new_storage;
+	   select * from new_storage; --select after commit on console 1 is ok
 	   commit;
-	   select * from storage; --updated
+	   select * from new_storage; --updated
 
 	   --Phantom read. Console 1
-	   begin;
-	   select * from storage;
-	   insert into storage(name) values ('five');
-	   commit;
-	   begin;
-	   select * from storage;
-	   insert into storage(name) values ('six');
+	   begin transaction isolation level serializable;
+	   select avg(weight) from new_storage;
+	   update new_storage set weight = 10 where id = 1;
 	   commit;
 
+
 	   --Phantom read. Console 2
-	   begin;
-	   select * from storage;
-	   select * from storage; --select after insert new item in console 1. ok
-	   select * from storage; --select after commit in console 1. not ok
-	   commit;
 	   begin transaction isolation level serializable;
-	   select * from storage;
-	   select * from storage; --select after insert new item in console 1. ok
-	   select * from storage; --select after commit in console 1. ok
+	   select avg(weight) from new_storage;
+	   update new_storage set weight = 100 where id = 2;
 	   commit;
