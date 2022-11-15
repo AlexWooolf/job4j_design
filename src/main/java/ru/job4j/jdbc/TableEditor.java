c378cada2fc8b7fbe8c68ea8e4fcb101ebd512f7
AlexWooolf <para_bellum@list.ru>
2022-11-15 13:26:33 +0300

    0.1. Statement [#379306 #360981]

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -85,7 +85,7 @@ public class TableEditor implements AutoCloseable {
 
     public static void main(String[] args) throws IOException {
         Properties config = new Properties();
-        try (InputStream in = TableEditor.class.getClassLoader().getResourceAsStream("\"src/main/resources/TableEditor.properties")) {
+        try (InputStream in = TableEditor.class.getClassLoader().getResourceAsStream("TableEditor.properties")) {
             config.load(in);
         }
         try (TableEditor tableEditor = new TableEditor(config)) {
